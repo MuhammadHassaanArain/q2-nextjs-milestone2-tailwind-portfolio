f56f94d2a75e4d97598c7db0b336498b9ff3af50
Hassaan Arain <rafatnaz21@gmail.com>
2024-11-06 13:34:18 +0500

    code pushed

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,12 +3,12 @@ function HomePage() {
   return (
     <main className="w-screen bg-bgdark py-14 flex justify-center">
       <section className="text-center md:w-3/5">
-        <h2 className="text-accentOrange text-4xl py-4 font-extrabold font-mono">Crafting Seamless Web Experiences</h2>
-        <h3 className="text-textLight text-2xl py-4">
+        <h2 className="text-accentOrange text-3xl md:text-4xl py-4 font-extrabold font-mono">Crafting Seamless Web Experiences</h2>
+        <h3 className="text-textLight text-xl md:text-2xl py-4">
           Full Stack Developer with Expertise in Next.js, TypeScript, and
           Tailwind CSS
         </h3>
-        <p className="text-textSecondary text-xl font-bold font-mono">
+        <p className="text-textSecondary md:text-xl font-bold font-mono">
           Hello! I&apos;m Muhammad Hassaan Arain, a developer passionate about
           building modern, responsive web applications. With expertise in
           Next.js and TypeScript, I create scalable and efficient web solutions.
